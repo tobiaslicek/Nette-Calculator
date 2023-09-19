ae651f78fa528d75e34fb85e27245639b1ebd2db
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-09-19 09:47:13 +0200

    make the second level of calculator

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -9,109 +9,64 @@ use Nette;
 
 final class HomePresenter extends Nette\Application\UI\Presenter
 {
+// protected function createComponentCalculatorForm()
+// {
+//     $form = new \Nette\Application\UI\Form;
+
+//     $form->addText('marze', 'MARŽE');
+//     $form->addText('profit', 'Hrubý zisk');
+//     $form->addText('reklamy', 'Reklamy');
+//     $form->addText('doprava', 'Doprava');
+
+//     $form->addSubmit('submit', 'Vypočítat');
+
+//     $form->onSuccess[] = [$this, 'calculatorFormSucceeded'];
+
+//     return $form;
+// }
+
+// public function calculatorFormSucceeded(\Nette\Application\UI\Form $form, \stdClass $values)
+// {
+//     $marzeValue = $values->profit - ($values->reklamy + $values->doprava);
+//     $form['marze']->setValue($marzeValue);
+// }
+
 protected function createComponentCalculatorForm()
 {
     $form = new \Nette\Application\UI\Form;
 
     $form->addText('marze', 'MARŽE');
     $form->addText('profit', 'Hrubý zisk');
-    $form->addText('reklamy', 'Reklamy');
+    $form->addText('google', 'Google Ads');
+    $form->addText('meta', 'Meta');
+    $form->addText('bing', 'Bing');
+    $form->addText('sklik', 'Sklik');
     $form->addText('doprava', 'Doprava');
 
     $form->addSubmit('submit', 'Vypočítat');
 
     $form->onSuccess[] = [$this, 'calculatorFormSucceeded'];
 
+    $session = $this->getSession('form');
+
+    if (isset($session->formValues->marze)) {
+        $form['marze']->setValue($session->formValues->marze);
+    }
+
     return $form;
 }
+
 public function calculatorFormSucceeded(\Nette\Application\UI\Form $form, \stdClass $values)
 {
-    $marzeValue = $values->profit - ($values->reklamy + $values->doprava);
+ 
+    $marzeValue = (float)$values->profit - ((float)$values->google + (float)$values->meta + (float)$values->bing + (float)$values->sklik + (float)$values->doprava);
+
     $form['marze']->setValue($marzeValue);
+
+    $session = $this->getSession('form');
+    $session->formValues = $values;
+
+    // $this->redirect('this');
 }
+
 }
-
-
-
-/* 
-    Verze 1
-
-    marže = hrubý zisk - Reklamy - Doprava
-    
-    
-    
-    
-    Verze 2
-    
-    marže = hrubý zisk - Google Ads - Meta - Bing - Sklik - Zásilkovna - Pošta - PPL 
-
-
-	<div class="ads">
-		<p class="ads__p"> - Reklamy</p>
-			<div class="google">
-				<p class="ads-google">Google Ads</p>
-				<input type="text" name="num1" placeholder="">
-			</div>
-			<div class="google">
-				<p class="ads-meta">Meta</p>
-				<input type="text" name="num1" placeholder="">
-				</div>
-			<div class="google">
-				<p class="ads-bing">Bing</p>
-				<input type="text" name="num1" placeholder="">
-				</div>
-			<div class="google">
-				<p class="ads-sklik">Sklik</p>
-				<input type="text" name="num1" placeholder="">
-			</div>
-	</div>
-
-
-	<div class="carrier">
-		<p class="carrier__p"> - Dopravce</p>
-			<div class="zasilkovna">
-				<h2 class="carrier-zasilkovna"><strong>Zásilkovna</strong></h2>
-				<p class="carrier-option">Výdejní místo</p>
-				<input type="text" name="num1" placeholder="">
-				<p class="carrier-option">Do ruky</p>
-				<input type="text" name="num1" placeholder="">
-			</div>
-			<div class="posta">
-				<h2 class="carrier-posta"><strong>Pošta</strong></h2>
-				<p class="carrier-option">Výdejní místo</p>
-				<input type="text" name="num1" placeholder="">
-				<p class="carrier-option">Do ruky</p>
-				<input type="text" name="num1" placeholder="">
-				<p class="carrier-option">Balíkovna</p>
-				<input type="text" name="num1" placeholder="">
-				</div>
-			<div class="ppl">
-				<h2 class="carrier-ppl"><strong>PPL</strong></h2>
-				<p class="carrier-option">Výdejní místo</p>
-				<input type="text" name="num1" placeholder="">
-				<p class="carrier-option">Do ruky</p>
-				<input type="text" name="num1" placeholder="">
-				</div>
-	</div>
-	<p class="order__p">Objednávek celkem</p>
-		<input type="text" name="num1" placeholder="celkem">
-
-
-
-
-
-
-
-
-
-
-
-
-
-    marže = hrubý zisk - $reklamy - poštovné
-
-    $reklamy = Google Ads + Meta + Bing + Sklik
-    $poštovné = Zásilkovna + Pošta + PPL 
-
-
-*/
