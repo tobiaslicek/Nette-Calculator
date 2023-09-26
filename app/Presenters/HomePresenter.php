7bc1dbd9f19657cdb5e03045d39665e425e8b866
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-09-26 10:42:32 +0200

    color the Kontrola word green or red according to Celkem

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-use Nette\Application\UI\Form; //přidáno správně? nebo smazat?
+use Nette\Application\UI\Form; 
 
 
 final class HomePresenter extends Nette\Application\UI\Presenter
