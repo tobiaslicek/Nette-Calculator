8b53d7aa35bd39f00f9b3de1490885bcdceed63a
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-09-26 15:49:02 +0200

    add optional table cells

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -41,20 +41,19 @@ protected function createComponentCalculatorForm()
 {
     $form = new \Nette\Application\UI\Form;
 
-    $form->addText('marze', 'MARŽE')->setHtmlAttribute('placeholder', 'Toto pole nechte volné');
-    $form->addText('profit', 'Hrubý zisk')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč');
-    $form->addText('google', 'Google Ads')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč');
-    $form->addText('meta', 'Meta')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč');
-    $form->addText('bing', 'Bing')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč');
-    $form->addText('sklik', 'Sklik')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč');
-    $form->addText('z_vydejni', 'Výdejní místo')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('z_doruky', 'Do ruky')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('p_vydejni', 'Výdejní místo')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('p_doruky', 'Do ruky')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('p_balikovna', 'Balíkovna')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('ppl_vydejni', 'Výdejní místo')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('ppl_doruky', 'Do ruky')->setHtmlAttribute('placeholder', 'Zadejte počet');
-    $form->addText('celkem', 'Objednávek celkem')->setHtmlAttribute('placeholder', 'Zadejte počet');
+    $form->addText('profit', 'Hrubý zisk')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč')->setRequired('Prosím, vyplňte pole "Hrubý zisk".');
+    $form->addText('google', 'Google Ads')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč')->setNullable();
+    $form->addText('meta', 'Meta')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč')->setNullable();
+    $form->addText('bing', 'Bing')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč')->setNullable();
+    $form->addText('sklik', 'Sklik')->setHtmlAttribute('placeholder', 'Zadejte hodnotu v Kč')->setNullable();
+    $form->addText('z_vydejni', 'Výdejní místo')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('z_doruky', 'Do ruky')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('p_vydejni', 'Výdejní místo')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('p_doruky', 'Do ruky')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('p_balikovna', 'Balíkovna')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('ppl_vydejni', 'Výdejní místo')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('ppl_doruky', 'Do ruky')->setHtmlAttribute('placeholder', 'Zadejte počet')->setNullable();
+    $form->addText('celkem', 'Objednávek celkem')->setHtmlAttribute('placeholder', 'Zadejte počet')->setRequired('Prosím, vyplňte pole "Objednávek celkem"');
     
     $form->addSubmit('submit', 'Vypočítat');
 
